355d0ed34d6292ab6cfff376cd349d014104ca9d
Diego Ventura <diegoventurasg@gmail.com>
2022-05-03 13:43:22 -0300

    add roboto font

--- lib/themes/app_text_styles.dart
+++ lib/themes/app_text_styles.dart
@@ -1,57 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app_colors.dart';
 
 class AppTextStyles {
-  static const bmiRange = TextStyle(
+  static final textSplash = GoogleFonts.roboto(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    color: AppColors.background,
+  );
+
+  static final bmiRange = GoogleFonts.roboto(
     fontSize: 25,
     fontWeight: FontWeight.w700,
     color: AppColors.secondary,
   );
 
-  static const bmiClassification = TextStyle(
+  static final bmiClassification = GoogleFonts.roboto(
     fontSize: 18,
     fontWeight: FontWeight.w700,
     color: AppColors.secondary,
   );
 
-  static const button = TextStyle(
+  static final button = GoogleFonts.roboto(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     color: AppColors.secondary,
   );
 
-  static const noResult = TextStyle(
+  static final noResult = GoogleFonts.roboto(
     fontSize: 32,
     fontWeight: FontWeight.w700,
     color: AppColors.secondary,
   );
 
-  static const result = TextStyle(
+  static final result = GoogleFonts.roboto(
     fontSize: 50,
     fontWeight: FontWeight.w700,
     color: AppColors.secondary,
   );
 
-  static const titleResult = TextStyle(
+  static final titleResult = GoogleFonts.roboto(
     fontSize: 14,
     fontWeight: FontWeight.w600,
     color: AppColors.secondary,
   );
 
-  static const description = TextStyle(
+  static final description = GoogleFonts.roboto(
     fontSize: 18,
     fontWeight: FontWeight.w400,
     color: AppColors.secondary,
   );
 
-  static const titleInput = TextStyle(
+  static final titleInput = GoogleFonts.roboto(
     fontSize: 18,
     fontWeight: FontWeight.w600,
     color: AppColors.secondary,
   );
 
-  static const input = TextStyle(
+  static final input = GoogleFonts.roboto(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     color: AppColors.secondary,
